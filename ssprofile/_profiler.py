f996e7c1ed932da2c62922c4850487151e8fe17c
Patrick Yang <patrick22414@outlook.com>
2020-08-31 16:15:29 +0800

    [_profiler] add ideas to profiler

--- ssprofile/_profiler.py
+++ ssprofile/_profiler.py
@@ -7,40 +7,53 @@ from _networks import SearchSpaceBaseNetwork
 class SearchSpaceProfiler:
     def __init__(
         self,
+        # params defining search spaces
         search_spaces: Dict[str, List[str]],  # eg {"ss0": ["block0", ...], "ss1": ...}
         default_blocks: Dict[str, str],  # eg {"ss0": "blockx", "ss1": ...}
         num_cell_groups: int,
         cell_layout: List[int],
         reduce_cell_groups: List[int],
+        # params for first training and finetuning
         first_train_epochs: int,
         first_train_optimizer_cfg: Dict,
+        first_train_scheduler_cfg: Dict,
         finetune_epochs: int,
         finetune_optimizer_cfg: Dict,
+        finetune_scheduler_cfg: Dict,
+        # other params required to make SSBNs
+        c_in_list: List[int],
+        c_out_list: List[int],
+        num_classes: int,
+        # others
         cost_latency_coeff: float,
         device: torch.device,
+        profile_dir: str,
+        # TODO: what else?
     ):
         super().__init__()
 
         self.search_spaces = search_spaces
+        self.default_blocks = default_blocks
+
+        self.num_cell_groups = num_cell_groups
+        self.cell_layout = cell_layout
+        self.reduce_cell_groups = reduce_cell_groups
 
         self.first_train_epochs = first_train_epochs
-        # self.first_train_optimizer = init_optimizer_from_cfg(first_train_optimizer_cfg)
+        self.first_train_optimizer_cfg = first_train_optimizer_cfg
+        self.first_train_scheduler_cfg = first_train_scheduler_cfg
+
         self.finetune_epochs = finetune_epochs
-        # self.finetune_optimizer = init_optimizer_from_cfg(finetune_optimizer_cfg)
+        self.finetune_optimizer_cfg = finetune_optimizer_cfg
+        self.finetune_scheduler_cfg = finetune_scheduler_cfg
 
-        self.ssnbs = []
-        for ss in self.search_spaces:
-            self.ssnbs.append(
-                SSBaseNetwork(
-                    primitives=self.search_spaces[ss],
-                    default_block=default_block[ss],
-                    num_cell_groups=num_cell_groups,
-                    cell_layout=cell_layout,
-                    reduce_cell_groups=reduce_cell_groups,
-                    # TODO
-                )
-            )
+        self.c_in_list = c_in_list
+        self.c_out_list = c_out_list
+        self.num_classes = num_classes
 
+        self.profile_accuracy_on_this_device = device
+
+        # TODO: how do we name each SSBN in each search space, ie keys in these dicts?
         self.accuracy_table: Dict[str, float] = {}
         self.latency_table: Dict[str, float] = {}
 
@@ -50,6 +63,26 @@ class SearchSpaceProfiler:
         # 3. SSBN first train and test
         # 4. SSBN swap blocks, finetune, and test
         # 5. write results to accuracy_table
+
+        # SSBNs are local to this method
+        # num of SSBNs == len(search_spaces)
+
+        # we need multiple optimizers/schedulers each time the SSBN changes
+
+        for ss in self.search_spaces:
+            # make SSBN
+            ssbn = SearchSpaceBaseNetwork(
+                primitives=self.search_spaces[ss],
+                default_block=self.default_blocks[ss],
+                num_cell_groups=self.num_cell_groups,
+                cell_layout=self.cell_layout,
+                reduce_cell_groups=self.reduce_cell_groups,
+                c_in_list=self.c_in_list,
+                c_out_list=self.c_out_list,
+                num_classes=self.num_classes,
+                device=self.profile_accuracy_on_this_device,
+            )
+            # first training
         pass
 
     def profile_latency(self):
@@ -61,11 +94,8 @@ class SearchSpaceProfiler:
         # sort and select search space and blocks
         pass
 
-    def first_train(self):
-        pass
-
-    def finetune(self):
-        pass
+    def get_ssbn_identifier(search_space_name, block_indices_for_each_cell_group):
+        return f"{search_space_name}_{'_'.join(block_indices_for_each_cell_group)}"
 
 
 def init_optimizer_from_cfg(cfg: Dict, module_parameters):
